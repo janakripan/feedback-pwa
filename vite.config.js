5995ff7a7c72f6d576318c178a9c41f4416398de
janakripan <janakripanjkc@gmail.com>
2025-03-20 13:37:32 +0530

    empty

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react(), VitePWA({
-    registerType: 'autoaUpdate',
+    registerType: 'autoUpdate',
     injectRegister: false,
 
     pwaAssets: {
